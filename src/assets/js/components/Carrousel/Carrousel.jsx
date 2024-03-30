65f78c3561f11fd2bea8fa4c7f886cbbe7272d02
matdelan <delannoy.mathieu@gmail.com>
2024-03-30 20:51:07 +0100

    feat: project finish

--- src/assets/js/components/Carrousel/Carrousel.jsx
+++ src/assets/js/components/Carrousel/Carrousel.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
 import './Carrousel.sass'
 
 export default function Carrousel({imgLinks}) {
+    const maxImg = imgLinks.length
+    const [currentImg, setCurrentImg] = useState(1)
 
-    switch(imgLinks.length) {
+    const handleClickAfter = () => {
+        let nb = currentImg + 1
+
+        if(nb == (maxImg+1))
+             nb = 1
+        setCurrentImg(nb)
+    }
+    const handleClickBefore = () => {
+        let nb = currentImg - 1
+
+        if(nb == 0)
+             nb = maxImg
+        setCurrentImg(nb)
+    }
+
+    switch(maxImg) {
         case "0":
-            throw new Error("Carrousel Componant : No image to loading")
+            throw new Error("Carrousel Component : No image to loading")
         case "1":
             return<>
                 <div className='carrousel'>
@@ -15,7 +33,10 @@ export default function Carrousel({imgLinks}) {
         default:
             return<>
                 <div className='carrousel'>
-                    <img className='carrousel__img' src={imgLinks[0]} alt="Rental image"/>
+                    <i onClick={handleClickBefore} className='fa-solid fa-chevron-left carrousel__inImg carrousel__arrow-left'></i>
+                    <p className='carrousel__inImg carrousel__count'>{currentImg}/{maxImg}</p>
+                    <img className='carrousel__img' src={imgLinks[currentImg -1]} alt="Rental image"/>
+                    <i onClick={handleClickAfter} className='fa-solid fa-chevron-right carrousel__inImg carrousel__arrow-right'></i>
                 </div>
             </>
             break
